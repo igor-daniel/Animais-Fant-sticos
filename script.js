6f1508c8d26bc694d31377f12beddf70d6486d37
igor-daniel <igord1870@gmail.com>
2021-08-10 00:34:10 -0300

    Area de fac mudada, e scroll suave nos links internos.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function initMenuTab(){
 const tabImg = document.querySelectorAll(".js-tabImg li");
 const tabContent = document.querySelectorAll(".js-tabContent section")
 
@@ -15,3 +16,42 @@ tabImg.forEach((link, index) => {
         tabClass(index)
     })
 })
+}
+initMenuTab()
+
+
+function initAcordin(){
+    const listaDT = document.querySelectorAll(".js-acordinList dt")
+
+listaDT[0].classList.add('ativo')
+listaDT[0].nextElementSibling.classList.add('ativo')
+
+function plusText(){
+    this.classList.toggle("ativo")
+    this.nextElementSibling.classList.toggle("ativo")
+}
+listaDT.forEach((el) => {
+    el.addEventListener('click', plusText);
+})
+}
+initAcordin()
+
+function initScrollSuave(){
+    const menu = document.querySelectorAll(".js-menu a[href^='#']")
+
+    function scrollSection(event){
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute("href");
+        const section = document.querySelector(href);
+        
+        section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+    })
+    }
+
+    menu.forEach((el)=> {
+    el.addEventListener("click", scrollSection);
+    })
+}
+initScrollSuave()
